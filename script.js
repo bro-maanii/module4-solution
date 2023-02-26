e5d4f5f62bb3562a0b7dffc6128ca5dcce75ab0b
Eman Ali Abbasi <125199246+bro-maanii@users.noreply.github.com>
2023-02-27 02:02:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+(function (){
+    var array =["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    console.log(array);
+    for (var i=0;i<array.length;i++){
+        if (array[i].charAt(0)==="J" || array[i].charAt(0)==="j"){
+            console.log("Good Bye " + array[i]);
+        } 
+        else {
+            console.log("Hello " + array[i]);
+        }
+    }
+})();
